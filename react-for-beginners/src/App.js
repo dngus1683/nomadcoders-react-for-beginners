87e0f61e3b83779c00de9be8a01693f9c547a2eb
dngus1683 <dngus1683@naver.com>
2022-10-24 18:39:55 +0900

    7.2 Coin Tracker

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -2,27 +2,52 @@ import './App.css';
 import {useState, useEffect} from 'react';
 
 function App() {
-  const [toDo, setToDo] = useState("");
-  const [toDos, setToDos] = useState([]);
-  const onChange = (event)=>setToDo(event.target.value);
-  const onSubmit = (event)=>{
-    event.preventDefault(); // preventDefault()로 form에서 submit이 발생할 때 새로 실행하지 않게 해준다.
-    if(toDo === '') return;
-    setToDo("");
-    setToDos((currentArray)=>[toDo, ...currentArray]);
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  const [usd, setUsd] = useState(0);
+  const [price, setPrice] = useState(0);
+  const [coin, setCoin] = useState(0);
+
+  const onChangeUsd = (event)=>{
+    setUsd(event.target.value);
+  }
+  const onChangeSelect = (event)=>{
+    setPrice(coins[event.target.value].quotes.USD.price);
+    setCoin(0);
+    setUsd(0);
+  }
+  const onClick = ()=>{
+    setCoin(usd/price);
   }
 
+  useEffect(()=>{
+    fetch("https://api.coinpaprika.com/v1/tickers")
+    .then((response)=> response.json())
+    .then((json)=> {
+      console.log(json);
+      setCoins(json);
+      setLoading(false);
+    });
+  },[]);
   return (
     <div className="App">
-      <h1>My To Dos ({toDos.length})</h1>
-      <form onSubmit={onSubmit}>
-        <input onChange={onChange} value={toDo} type="text" placeholder='Write your to do...'/>
-        <button>Add To Do</button>
-      </form>
-      <hr/>
-      <ul>
-        {toDos.map((item, index)=><li key={index}>{item}</li>)}
-      </ul>
+      <h1>The Coins! {loading ? "":  `(${coins.length})`}</h1>
+      {loading ? <strong>Loading...</strong> : 
+        <div>
+          <select onChange={onChangeSelect}>
+            {coins.map((coin, index)=>(
+              <option key={index} value={index}>{coin.name} ({coin.symbol}): {coin.quotes.USD.price} USD</option>
+            ))}
+          </select>
+          <div>
+            <input type='number' onChange={onChangeUsd} value={usd}/> 
+            <button onClick={onClick}>cal</button>
+          </div>
+          <hr/>
+          {coin}
+        </div>
+        
+      }      
     </div>
   );
 }
